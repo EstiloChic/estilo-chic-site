0a34d050f9315d90b1baf45b455b4872f6853f6c
EstiloChic <estilochicccc@gmail.com>
2025-08-05 17:05:40 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,14 @@ module.exports = function(eleventyConfig) {
   // Copia a pasta 'admin' para a pasta de saída '_site'
   eleventyConfig.addPassthroughCopy("admin");
 
+  // NOVO: Filtro personalizado para formatar números como preço (ex: 30 -> 30,00)
+  eleventyConfig.addFilter("formatPrice", function(value) {
+    if (typeof value !== 'number') {
+      return value;
+    }
+    return Number(value).toFixed(2).replace('.', ',');
+  });
+
   return {
     // Estas são as pastas padrão, mas é bom tê-las aqui
     dir: {
